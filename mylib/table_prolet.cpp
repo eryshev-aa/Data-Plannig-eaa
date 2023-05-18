d44869ca1392085814080d517956672e16a2e1b4
Thg-Prog <truxin2005@mail.ru>
2023-05-18 22:46:01 +0300

    fix

--- mylib/table_prolet.cpp
+++ mylib/table_prolet.cpp
@@ -5,8 +5,8 @@ table_prolet::table_prolet()
 
 };
 double table_prolet::TimeDifference(table_prolet::TimeZone zone1, table_prolet::TimeZone zone2){
-    time_t first=mktime(&(zone1.tm))*1000+zone1.milisecs;
-    time_t second=mktime(&(zone2.tm))*1000+zone2.milisecs;
+    time_t first=mktime(&(zone1.tm_start))*1000+zone1.milisecs_start;
+    time_t second=mktime(&(zone2.tm_start))*1000+zone2.milisecs_start;
     double differ= (first-second)/1000;
     return differ;
 };
